9297970f947d6df5b20353dd3db7e0d8d91894f6
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-03-18 22:32:13 +0600

    hero top section done

---
+++ src/compontnts/TopLeftCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import "./TopLeftCard.css"
+export default function TopLeftCard() {
+  return (
+    <div className='card-container'>
+        
+    </div>
+  )
+}
--- src/pages/sections/Hero/Hero.tsx
+++ src/pages/sections/Hero/Hero.tsx
@@ -1,17 +1,23 @@
-import React from 'react'
-import "./Hero.css"
+import React from "react";
+import "./Hero.css";
 
 export default function Hero() {
   return (
-    <div className='hero-container'>
-        <div className="title">
-            <h2 className='heading'>Technical Project Management</h2>
-            <button>Submit task</button>
-        </div>
-        {/* card part */}
-        <div className="details-card">
-            
-        </div>
+    <div className="hero-container">
+      <div className="title">
+        <h2 className="heading">Technical Project Management</h2>
+        <button>Submit task</button>
+      </div>
+      {/* card part */}
+      <div className="details-card">
+        <h3 className="card-heading">Explore the world of management</h3>
+        <p>
+          As a project manager, you play an important reole in leading a project
+          through initiation, planning, execution, monitoring, controlling and
+          completion. How? Do you want to manage each and every step of your
+          life?
+        </p>
+      </div>
     </div>
-  )
+  );
 }
