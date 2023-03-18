a00355b309fcfd30daf0068fe9165b7c04842342
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-03-18 22:21:21 +0600

    header added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
+import Hero from './sections/Hero/Hero'
 import Nav from './sections/Nav/Nav'
 
 export default function Home() {
   return (
     <>
     <Nav></Nav>
+    <div className="">
+        <Hero/>
+    </div>
     </>
   )
 }
---
+++ src/pages/sections/Hero/Hero.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import "./Hero.css"
+
+export default function Hero() {
+  return (
+    <div className='hero-container'>
+        <div className="title">
+            <h2 className='heading'>Technical Project Management</h2>
+            <button>Submit task</button>
+        </div>
+        {/* card part */}
+        <div className="details-card">
+            
+        </div>
+    </div>
+  )
+}
