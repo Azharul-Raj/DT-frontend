b4d23a0fca493d8fc5c6ee5d716b46f2a61450f0
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-03-20 06:16:30 +0600

    readme added

--- src/compontnts/FloatIcons/FloatIcons.tsx
+++ src/compontnts/FloatIcons/FloatIcons.tsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import {MdQuestionMark} from 'react-icons/md'
-import {FaRegCalendarAlt,FaPeopleCarry} from 'react-icons/fa'
+
+import { MdQuestionMark } from 'react-icons/md'
+import { FaRegCalendarAlt, FaPeopleCarry } from 'react-icons/fa'
 import "./FloatIcons.css"
 
 export default function FloatIcons() {
   return (
     <div className='main-float'>
       <div className="float-icons-container">
-      <div className="float-icon-container">
-      <MdQuestionMark className='float-icon'/>
-      </div>
-      <div className="float-icon-container">
-      <FaPeopleCarry className='float-icon'/>
-      </div>
-      <div className="float-icon-container">
-      <FaRegCalendarAlt className='float-icon'/>
-      </div>
+        <div className="float-icon-container">
+          <MdQuestionMark className='float-icon' />
+        </div>
+        <div className="float-icon-container">
+          <FaPeopleCarry className='float-icon' />
+        </div>
+        <div className="float-icon-container">
+          <FaRegCalendarAlt className='float-icon' />
+        </div>
       </div>
     </div>
   )
